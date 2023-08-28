3bbe0b03a7b13d51dd3e1595ffbae527bed2ed8f
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 14:26:50 -0600

    Use a different method with less code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Form from "./Form";
 
-var userIsRegistered = true;
+var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      <Form />
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,8 +6,10 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {!props.isRegistered && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
