6f40ba4b50e93d24c01612a82c993b6cd516fbb0
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 08:06:10 -0600

    Solve out the issue with the app using npm i react-scripts@latest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "../styles.css";
+
+const isLoggedIn = false;
+
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  if (isLoggedIn) {
+    return (
+      <div className="container">
+        <h1>Hello</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
