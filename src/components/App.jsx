c4199d4e43bc9ec0f62435e3fb7d4be619ec9df7
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 11:47:26 -0600

    conditional rendering, if user is registered or if user isn't registred.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
 import React from "react";
-import Form from "./Form";
+import "../styles.css";
 
 var userIsRegistered = false;
 
-//Challenge: Without moving the userIsRegistered variable,
-//1. Show Login as the button text if userIsRegistered is true.
-//Show Register as the button text if userIsRegistered is false.
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
+// Challenge:
+// 1. Show "Login" as the button text if userIsRegistered is true.
+//    Show "Register" as the button text if userIsRegistered is false.
+// 2. Only show the "Confirm Password" input if userIsRegistered is false.
+//    Don't show it if userIsRegistered is true.
+
+function CheckRegisteredUser({ userIsRegistered }) {
+  return (
+    <div>
+      {userIsRegistered ? (
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      ) : (
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <button type="submit">Register</button>
+        </form>
+      )}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="container">
-      <Form />
+      <CheckRegisteredUser userIsRegistered={userIsRegistered} />
     </div>
   );
 }
