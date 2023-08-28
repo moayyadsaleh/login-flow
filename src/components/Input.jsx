d90b43a6458baf195b3b351128b84f6cdaf268a7
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-28 08:17:47 -0600

    created separate components for the login form and for individual input fields

---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./App";
+
+function InputField({ type, placeholder }) {
+  return <input type={type} placeholder={placeholder} />;
+}
+
+export default InputField;
---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import App from "./App";
+import InputField from "./Input";
+
+function Login() {
+  return (
+    <div className="container">
+      <form className="form">
+        <InputField type="text" placeholder="Username" />
+        <InputField type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
